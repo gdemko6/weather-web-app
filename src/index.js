bda2fe2c83cca870294525497fe022445711cef5
Greg Demko <gregory.demko@my.uwrf.edu>
2024-10-18 11:39:43 -0500

    Succesfully display 7 days of the week and their temperature to the DOM based on a search. Need to make display better

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ import { View } from "./view";
 import { WeatherAPI } from "./weatherAPI";
 import { WeatherType } from "./weatherType";
 
-window.addEventListener("load", initialize());
+window.addEventListener("load", initialize);
 
 async function initialize() {
   const view = new View();
   const weatherAPI = new WeatherAPI();
-  let minneapolisWeather = await weatherAPI.getWeather("Minneapolis");
-  view.displaySearchResults("Minneapolis", minneapolisWeather);
+
+  // Fetch initial weather data
+  const { weatherWeek, dateWeek } = await weatherAPI.getWeather("Minneapolis");
+  view.displaySearchResults("Minneapolis", weatherWeek, dateWeek);
 
   const searchButton = document.querySelector("#search-button");
   searchButton.addEventListener("click", async () => {
-    const search = document.querySelector("#search-input");
-    let currentWeather = await weatherAPI.getWeather(search.value);
-    view.displaySearchResults(search.value, currentWeather);
+    const searchInput = document.querySelector("#search-input");
+    const { weatherWeek, dateWeek } = await weatherAPI.getWeather(
+      searchInput.value
+    );
+
+    view.displaySearchResults(searchInput.value, weatherWeek, dateWeek);
+    searchInput.value = ""; // Clear the input field
   });
 }
